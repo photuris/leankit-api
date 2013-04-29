67dfcfef49facdaaf942e9871765139071379869
Joshua Pruitt <Joshua.Pruitt@hcahealthcare.com>
2013-04-29 13:22:09 -0500

    added get_identifiers method to 'board' module

--- lib/WWW/LeanKit/API/Board.pm
+++ lib/WWW/LeanKit/API/Board.pm
@@ -23,7 +23,7 @@ returns: ref {Array} List of boards
 sub all {
 	my $self = shift;
 
-	return $self->base->request->get('Boards/');
+	return $self->base->request->get('Boards');
 }
 
 =item B<find()>
@@ -40,7 +40,24 @@ sub find {
 	my $self   = shift;
 	my ( $id ) = @_;
 
-	return $self->base->request->get("Boards/$id/");
+	return $self->base->request->get("Boards/$id");
+}
+
+=item B<get_identifiers()>
+
+Find a identifiers for specified Board ID.
+
+param: scalar {Integer} Board ID
+
+returns: ref {Hash} The board identifiers
+
+=cut
+
+sub get_identifiers {
+	my $self   = shift;
+	my ( $id ) = @_;
+
+	return $self->base->request->get("Board/$id/GetBoardIdentifiers");
 }
 
 =back
