ca922b15c462fdabd00a07db69d116743fd9e6de
Joshua Pruitt <Joshua.Pruitt@hcahealthcare.com>
2013-04-29 13:45:19 -0500

    methods for 'archive' module

--- lib/WWW/LeanKit/API/Archive.pm
+++ lib/WWW/LeanKit/API/Archive.pm
@@ -4,4 +4,35 @@ use warnings;
 use strict;
 use Carp;
 
+=head1 NAME
+
+WWW::LeanKit::API::Archive
+
+=head1 METHODS
+
+=over 4
+
+=item B<fetch()>
+
+Find archive by specified Board ID.
+
+param: scalar {Integer} Board ID
+
+returns: ref {Hash} The archive
+
+=cut
+
+sub fetch {
+	my $self = shift;
+	my ( $board_id, $card_id ) = @_;
+
+	return $self->base->request->get("/Board/$board_id/Archive");
+}
+
+=back
+
+=cut
+
 1;
+
+__END__
